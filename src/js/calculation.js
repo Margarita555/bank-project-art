869a6aedd5e19dbbc96d9592ae9b1e908f407b91
Margarita555 <a_rita@ukr.net>
2022-02-24 20:50:42 +0200

    added files

--- src/js/calculation.js
+++ src/js/calculation.js
@@ -1,23 +1,14 @@
-// // import ApiService from './api-service';
 import fetchCurrencyRates from './api-service';
-// const form = document.querySelector('.form');
-// const debitForm = document.querySelector('.debitForm');
-// const creditForm = document.querySelector('.creditForm');
-// const totalFundsBtn = document.querySelector('#totalFunds');
-// const totalFunds = document.querySelector('.totalFunds');
+import getRefs from './refs';
+const { totalFundsBtn, totalFunds } = getRefs();
 
-// // const apiService = new ApiService();
-
-// const bank = apiService.clients;
 let bank = [];
 let clients = localStorage.getItem('bank');
 if (clients !== null) {
   bank = JSON.parse(clients);
 }
-// const bankContainer = document.querySelector('.bankContainer');
-// renderBank(bank);
 
-// totalFundsBtn.addEventListener('click', countBankCashTotal);
+totalFundsBtn.addEventListener('click', countBankTotalFunds);
 
 async function exchangeCurrency(balance, currency) {
   const rate = await fetchCurrencyRates().then(rates => {
@@ -26,7 +17,7 @@ async function exchangeCurrency(balance, currency) {
   return (balance / 100) * rate;
 }
 
-async function countBankCashTotal() {
+async function countBankTotalFunds() {
   try {
     let debitTotal = 0;
     for (let i = 0; i < bank.length; i++) {
@@ -60,26 +51,32 @@ async function countBankCashTotal() {
     }
     const total = debitTotal + creditTotal;
     totalFunds.innerText = total.toFixed(2);
-    console.log(total);
+    // console.log(total);
     return total;
   } catch (e) {
     error({ text: 'Error.Try again leter.' });
   }
 }
-countBankCashTotal();
+// countBankCashTotal();
 
-async function countClientsDebt() {
+async function countClientsDebt(status) {
   try {
     let debtTotal = 0;
     for (let i = 0; i < bank.length; i++) {
+      let flag = bank[i].isActive || !bank[i].isActive;
+      if (status && status === 'active') {
+        flag = bank[i].isActive;
+      }
+      if (status && status === 'inactive') {
+        flag = !bank[i].isActive;
+      }
       for (let j = 0; j < bank[i].accounts.credit.length; j++) {
         let account = bank[i].accounts.credit[j];
         let debt = 0;
-        if (Number(account.creditLimit) > Number(account.balance)) {
+        if (flag && Number(account.creditLimit) > Number(account.balance)) {
           debt = Number(account.creditLimit) - Number(account.balance);
         }
         let currency = account.currency;
-
         if (currency === 'USD') {
           debtTotal += debt;
         } else {
@@ -93,87 +90,27 @@ async function countClientsDebt() {
     error({ text: 'Error.Try again leter.' });
   }
 }
-countClientsDebt();
 
-async function countInactiveActiveClientsDebtFunds() {
-  try {
-    const total = 0;
-    for (let i = 0; i < bank.length; i++) {
-      for (let j = 0; j < bank[i].accounts.credit.length; j++) {
-        let account = bank[i].accounts.credit[j];
-        let debt = 0;
-        // let flag =
-        //  console.log(account.balance)
-        if (!bank[i].isActive && Number(account.creditLimit) > Number(account.balance)) {
-          debt += Number(account.creditLimit) - Number(account.balance);
-          let currency = account.currency;
-          console.log(debt);
-          if (currency === 'USD') {
-            total += debt;
-          } else {
-            const exchangedDebt = await exchangeCurrency(debt, currency);
-            console.log(exchangedDebt);
-            // total += exchangedDebt
-          }
-        }
-      }
-    }
+// countClientsDebt();
+// countClientsDebt('active');
+// countClientsDebt('inactive');
 
-    //     if(bank[i].isActive){
-    //       console.log('active')
-    //     //  break;
-    //     }
-
-    console.log(total);
-
-    return total;
-  } catch (e) {
-    error({ text: 'Error.Try again leter.' });
-  }
-}
-countInactiveActiveClientsDebtFunds();
-
-function countInactiveDebtHolders() {
-  // const inactiveClients = bank.filter((client) => {
-  //   return client.isActive && client.accounts.credit.balance.creditFunds !== 0;
-  // }).length;
+function countDebtHolders(status) {
   let counter = 0;
   for (let i = 0; i < bank.length; i++) {
-    let debt = false;
+    let flag = bank[i].isActive;
+    if (status && status === 'inactive') {
+      flag = !bank[i].isActive;
+    }
     for (let j = 0; j < bank[i].accounts.credit.length; j++) {
       let account = bank[i].accounts.credit[j];
-      if (account.balance.creditFunds > account.balance.personalFunds) {
-        debt = true;
-        break;
+      if (flag && Number(account.creditLimit) > Number(account.balance)) {
+        counter += 1;
       }
     }
-    if (debt && !bank[i].isActive) {
-      counter += 1;
-      debt = false;
-    }
   }
-  console.log(counter);
+  // console.log(counter);
   return counter;
 }
-// countInactiveDebtHolders();
-
-function countActiveDebtHolders() {
-  let counter = 0;
-  for (let i = 0; i < bank.length; i++) {
-    let debt = false;
-    for (let j = 0; j < bank[i].accounts.credit.length; j++) {
-      let account = bank[i].accounts.credit[j];
-      if (account.balance.creditFunds > account.balance.personalFunds) {
-        debt = true;
-        break;
-      }
-    }
-    if (debt && bank[i].isActive) {
-      counter += 1;
-      debt = false;
-    }
-  }
-  console.log(counter);
-  return counter;
-}
-// countActiveDebtHolders()
+// countDebtHolders('active');
+// countDebtHolders('inactive');
--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,8 @@ import bankTemplate from '../templates/bank-markup.hbs';
 import debitTemplate from '../templates/debit-markup.hbs';
 import creditTemplate from '../templates/credit-markup.hbs';
 import { nanoid } from 'nanoid';
-
-const form = document.querySelector('.form');
-const debitForm = document.querySelector('.debitForm');
-const creditForm = document.querySelector('.creditForm');
-const totalFundsBtn = document.querySelector('#totalFunds');
-const totalFunds = document.querySelector('.totalFunds');
-const bankContainer = document.querySelector('.bankContainer');
+import getRefs from './refs';
+const { form, debitForm, creditForm, bankContainer } = getRefs();
 
 let bank = [];
 let clients = localStorage.getItem('bank');
@@ -18,26 +13,28 @@ if (clients !== null) {
 
 renderBank(bank);
 
+form.addEventListener('submit', onFormSubmit);
+debitForm.addEventListener('submit', onDebitFormSubmit);
+creditForm.addEventListener('submit', onCreditFormSubmit);
+bankContainer.addEventListener('click', onDeleteBtnClick);
+bankContainer.addEventListener('click', onDebitAccountClick);
+bankContainer.addEventListener('click', onCreditAccountClick);
+bankContainer.addEventListener('click', saveClientData);
+bankContainer.addEventListener('click', saveDebitAccount);
+bankContainer.addEventListener('click', saveCreditAccount);
+
 function renderBank(bank) {
   bankContainer.innerHTML = '';
   const bankMarkup = bankTemplate(bank);
   bankContainer.insertAdjacentHTML('beforeend', bankMarkup);
 }
 
-form.addEventListener('submit', onFormSubmit);
-debitForm.addEventListener('submit', onDebitFormSubmit);
-creditForm.addEventListener('submit', onCreditFormSubmit);
-// totalFundsBtn.addEventListener('click', countBankCashTotal);
-bankContainer.addEventListener('click', onDeleteBtnClick);
-bankContainer.addEventListener('click', onDebitAccountClick);
-bankContainer.addEventListener('click', onCreditAccountClick);
-
 function onFormSubmit(event) {
   event.preventDefault();
-  const formElements = event.currentTarget.elements;
-  const name = formElements.name.value;
-  const registrationDate = formElements.date.value;
-  const isActive = formElements.isActive.value;
+  const formData = new FormData(event.target.closest('form'));
+  const name = formData.get('name');
+  const registrationDate = formData.get('date');
+  const isActive = formData.get('isActive');
   const id = nanoid();
   const client = {
     id,
@@ -52,20 +49,20 @@ function onFormSubmit(event) {
   bank.push(client);
   localStorage.setItem('bank', JSON.stringify(bank));
   renderBank(bank);
-
   form.reset();
 }
 
 function onDebitFormSubmit(event) {
   event.preventDefault();
-  const formElements = event.currentTarget.elements;
+  const formData = new FormData(event.target.closest('form'));
 
-  const id = formElements.id.value;
-  const balance = formElements.balance.value;
-  const activity = formElements.activity.value;
-  const activityDate = formElements.activityDate.value;
-  const cardExpiryDate = formElements.cardExpiryDate.value;
-  const currency = formElements.currency.value;
+  const id = formData.get('id');
+  const balance = formData.get('balance');
+  const activity = formData.get('activity');
+  const activityDate = formData.get('activityDate');
+  const cardExpiryDate = formData.get('cardExpiryDate');
+  const currency = formData.get('currency');
+  const accountId = formData.get('accountId');
 
   const debitAccount = {
     balance,
@@ -73,6 +70,8 @@ function onDebitFormSubmit(event) {
     activityDate,
     cardExpiryDate,
     currency,
+    id,
+    accountId,
   };
   const client = bank.find(client => id == client.id);
   client.accounts.debit.push(debitAccount);
@@ -83,15 +82,16 @@ function onDebitFormSubmit(event) {
 
 function onCreditFormSubmit(event) {
   event.preventDefault();
-  const formElements = event.currentTarget.elements;
+  const formData = new FormData(event.target.closest('form'));
 
-  const id = formElements.id.value;
-  const balance = formElements.balance.value;
-  const activity = formElements.activity.value;
-  const activityDate = formElements.activityDate.value;
-  const cardExpiryDate = formElements.cardExpiryDate.value;
-  const currency = formElements.currency.value;
-  const creditLimit = formElements.creditLimit.value;
+  const id = formData.get('id');
+  const balance = formData.get('balance');
+  const activity = formData.get('activity');
+  const activityDate = formData.get('activityDate');
+  const cardExpiryDate = formData.get('cardExpiryDate');
+  const currency = formData.get('currency');
+  const creditLimit = formData.get('creditLimit');
+  const accountId = formData.get('accountId');
 
   const creditAccount = {
     balance,
@@ -100,11 +100,12 @@ function onCreditFormSubmit(event) {
     activityDate,
     cardExpiryDate,
     currency,
+    id,
+    accountId,
   };
 
   const client = bank.find(client => id == client.id);
   client.accounts.credit.push(creditAccount);
-  console.log(bank);
   localStorage.setItem('bank', JSON.stringify(bank));
   creditForm.reset();
 }
@@ -135,14 +136,74 @@ function onCreditAccountClick(event) {
   accountsContainer.insertAdjacentHTML('beforeend', creditMarkup);
 }
 
+function saveClientData(event) {
+  if (!event.target.hasAttribute('data-save')) {
+    return;
+  }
+  event.preventDefault();
+  const id = event.target.getAttribute('data-save');
+  let client = bank.find(client => client.id === id);
+
+  const formData = new FormData(event.target.closest('form'));
+  client.name = formData.get('name');
+  client.registrationDate = formData.get('date');
+  client.isActive = formData.get('isActive');
+
+  localStorage.setItem('bank', JSON.stringify(bank));
+}
+
 function onDeleteBtnClick(event) {
   if (!event.target.hasAttribute('data-remove')) {
     return;
   }
   const id = event.target.getAttribute('data-remove');
-
   const updatedBank = bank.filter(client => client.id !== id);
   bank = updatedBank;
   localStorage.setItem('bank', JSON.stringify(updatedBank));
   renderBank(updatedBank);
+}
+
+function saveDebitAccount(event) {
+  if (!event.target.hasAttribute('data-debitsave')) {
+    return;
+  }
+  event.preventDefault();
+
+  const id = event.target.getAttribute('id');
+  const client = bank.find(client => client.id === id);
+  const accountId = event.target.getAttribute('data-debitsave');
+  const account = client.accounts.debit.find(account => account.accountId === accountId);
+
+  const formData = new FormData(event.target.closest('form'));
+  account.balance = formData.get('balance');
+  account.activity = formData.get('activity');
+  account.currency = formData.get('currency');
+  account.activityDate = formData.get('activityDate');
+  account.cardExpiryDate = formData.get('cardExpiryDate');
+  account.accountId = formData.get('accountId');
+
+  localStorage.setItem('bank', JSON.stringify(bank));
+}
+
+function saveCreditAccount(event) {
+  if (!event.target.hasAttribute('data-creditsave')) {
+    return;
+  }
+  event.preventDefault();
+  const id = event.target.getAttribute('id');
+  const client = bank.find(client => client.id === id);
+  const accountId = event.target.getAttribute('data-creditsave');
+  const account = client.accounts.credit.find(account => account.accountId === accountId);
+
+  const formData = new FormData(event.target.closest('form'));
+
+  account.balance = formData.get('balance');
+  account.creditLimit = formData.get('creditLimit');
+  account.activity = formData.get('activity');
+  account.currency = formData.get('currency');
+  account.activityDate = formData.get('activityDate');
+  account.cardExpiryDate = formData.get('cardExpiryDate');
+  account.accountId = formData.get('accountId');
+
+  localStorage.setItem('bank', JSON.stringify(bank));
 }
